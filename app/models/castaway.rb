e582e866995410300cb07c61aa4e9c6c96e91479
Kuba Subczynski <kubasub12@gmail.com>
2018-02-15 09:10:05 -0500

    Display rank and totals on standings page

--- app/models/castaway.rb
+++ app/models/castaway.rb
@@ -1,4 +1,5 @@
 class Castaway < ApplicationRecord
   has_many :memberships
   has_many :teams, through: :memberships
+  has_many :scoring_events
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ApplicationRecord
   has_many :memberships
   has_many :castaways, through: :memberships
+
+  def points
+    castaways.flat_map(&:scoring_events).sum(&:points)
+  end
 end

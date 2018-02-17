0a494379b6c9cfef81874c18ecef335b3133a93f
Kuba Subczynski <kubasub12@gmail.com>
2018-02-17 01:54:47 -0500

    Applied Bootstrap to standings page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get :standings, to: "standings#index"
   get :scoring_breakdown, to: "scoring_breakdown#index"
 
-  namespace :adminz do
+  namespace :admin do
     resources :scoring_events
   end
 end

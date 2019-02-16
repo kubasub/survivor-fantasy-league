c5c0121cc91fca4baba6c1dc0e6d288f2e28c7c2
Kuba Subczynski <kubasub12@gmail.com>
2019-02-15 22:06:01 -0500

    seed season 38 data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,31 +17,62 @@ ScoringCriterion.create([
   { name: "Won Survivor", points: 12 },
 ])
 
+# Season 36
+# Episode.create([
+#   { number: 1, title: "Can You Reverse the Curse?" },
+#   { number: 2 },
+#   { number: 3 },
+# ])
+
+# Season 38
 Episode.create([
-  { number: 1, title: "Can You Reverse the Curse?" },
-  { number: 2 },
-  { number: 3 },
+  { number: 1, title: "It Smells Like Success" },
+  { number: 2, title: "One of Us is Going to Win the War" },
+  { number: 3, title: "Betrayals Are Going to Get Exposed" },
 ])
 
+# Season 36
+# Castaway.create([
+#   { first_name: "Domenick", last_name: "Abbate", headshot_filename: "domenick_abbate.jpg" },
+#   { first_name: "Desiree", last_name: "Afuye", headshot_filename: "desiree_afuye.jpg" },
+#   { first_name: "Kellyn", last_name: "Bechtold", headshot_filename: "kellyn_bechtold.jpg" },
+#   { first_name: "Jenna", last_name: "Bowman", headshot_filename: "jenna_bowman.jpg" },
+#   { first_name: "Jacob", last_name: "Derwin", headshot_filename: "jacob_derwin.jpg" },
+#   { first_name: "Stephanie", last_name: "Gonzalez", headshot_filename: "stephanie_gonzalez.jpg" },
+#   { first_name: "Wendell", last_name: "Holland", headshot_filename: "wendell_holland.jpg" },
+#   { first_name: "Donathan", last_name: "Hurley", headshot_filename: "donathan_hurley.jpg" },
+#   { first_name: "Laurel", last_name: "Johnson", headshot_filename: "laurel_johnson.jpg" },
+#   { first_name: "Stephanie", last_name: "Johnson", headshot_filename: "stephanie_johnson.jpg" },
+#   { first_name: "Bradley", last_name: "Kleihege", headshot_filename: "bradley_kleihege.jpg" },
+#   { first_name: "James", last_name: "Lim", headshot_filename: "james_lim.jpg" },
+#   { first_name: "Chris", last_name: "Noble", headshot_filename: "chris_noble.jpg" },
+#   { first_name: "Sebastian", last_name: "Noel", headshot_filename: "sebastian_noel.jpg" },
+#   { first_name: "Angela", last_name: "Perkins", headshot_filename: "angela_perkins.jpg" },
+#   { first_name: "Morgan", last_name: "Ricke", headshot_filename: "morgan_ricke.jpg" },
+#   { first_name: "Brendan", last_name: "Shapiro", headshot_filename: "brendan_shapiro.jpg" },
+#   { first_name: "Chelsea", last_name: "Townsend", headshot_filename: "chelsea_townsend.jpg" },
+#   { first_name: "Libby", last_name: "Vincek", headshot_filename: "libby_vincek.jpg" },
+#   { first_name: "Michael", last_name: "Yerger", headshot_filename: "michael_yerger.jpg" },
+# ])
+
+# Season 38
 Castaway.create([
-  { first_name: "Domenick", last_name: "Abbate", headshot_filename: "domenick_abbate.jpg" },
-  { first_name: "Desiree", last_name: "Afuye", headshot_filename: "desiree_afuye.jpg" },
-  { first_name: "Kellyn", last_name: "Bechtold", headshot_filename: "kellyn_bechtold.jpg" },
-  { first_name: "Jenna", last_name: "Bowman", headshot_filename: "jenna_bowman.jpg" },
-  { first_name: "Jacob", last_name: "Derwin", headshot_filename: "jacob_derwin.jpg" },
-  { first_name: "Stephanie", last_name: "Gonzalez", headshot_filename: "stephanie_gonzalez.jpg" },
-  { first_name: "Wendell", last_name: "Holland", headshot_filename: "wendell_holland.jpg" },
-  { first_name: "Donathan", last_name: "Hurley", headshot_filename: "donathan_hurley.jpg" },
-  { first_name: "Laurel", last_name: "Johnson", headshot_filename: "laurel_johnson.jpg" },
-  { first_name: "Stephanie", last_name: "Johnson", headshot_filename: "stephanie_johnson.jpg" },
-  { first_name: "Bradley", last_name: "Kleihege", headshot_filename: "bradley_kleihege.jpg" },
-  { first_name: "James", last_name: "Lim", headshot_filename: "james_lim.jpg" },
-  { first_name: "Chris", last_name: "Noble", headshot_filename: "chris_noble.jpg" },
-  { first_name: "Sebastian", last_name: "Noel", headshot_filename: "sebastian_noel.jpg" },
-  { first_name: "Angela", last_name: "Perkins", headshot_filename: "angela_perkins.jpg" },
-  { first_name: "Morgan", last_name: "Ricke", headshot_filename: "morgan_ricke.jpg" },
-  { first_name: "Brendan", last_name: "Shapiro", headshot_filename: "brendan_shapiro.jpg" },
-  { first_name: "Chelsea", last_name: "Townsend", headshot_filename: "chelsea_townsend.jpg" },
-  { first_name: "Libby", last_name: "Vincek", headshot_filename: "libby_vincek.jpg" },
-  { first_name: "Michael", last_name: "Yerger", headshot_filename: "michael_yerger.jpg" },
+  { first_name: "Aubry", last_name: "Bracco", headshot_filename: "aubry_bracco.jpg" },
+  { first_name: "Aurora", last_name: "McCreary", headshot_filename: "aurora_mccreary.jpg" },
+  { first_name: "Chris", last_name: "Underwood", headshot_filename: "chris_underwood.jpg" },
+  { first_name: "Dan", last_name: "DaSilva", headshot_filename: "dan_dasilva.jpg" },
+  { first_name: "David", last_name: "Wright", headshot_filename: "david_wright.jpg" },
+  { first_name: "Eric", last_name: "Hafemann", headshot_filename: "eric_hafemann.jpg" },
+  { first_name: "Gavin", last_name: "Whitson", headshot_filename: "gavin_whitson.jpg" },
+  { first_name: "Joe", last_name: "Anglim", headshot_filename: "joe_anglim.jpg" },
+  { first_name: "Julia", last_name: "Carter", headshot_filename: "julia_carter.jpg" },
+  { first_name: "Julie", last_name: "Rosenberg", headshot_filename: "julie_rosenberg.jpg" },
+  { first_name: "Keith", last_name: "Sowell", headshot_filename: "keith_sowell.jpg" },
+  { first_name: "Kelly", last_name: "Wentworth", headshot_filename: "kelly_wentworth.jpg" },
+  { first_name: "Lauren", last_name: "O'Connell", headshot_filename: "lauren_oconnell.jpg" },
+  { first_name: "Reem", last_name: "Daly", headshot_filename: "reem_daly.jpg" },
+  { first_name: "Rick", last_name: "Devens", headshot_filename: "rick_devens.jpg" },
+  { first_name: "Ron", last_name: "Clark", headshot_filename: "ron_clark.jpg" },
+  { first_name: "Victoria", last_name: "Baamonde", headshot_filename: "victoria_baamonde.jpg" },
+  { first_name: "Wendy", last_name: "Diaz", headshot_filename: "wendy_diaz.jpg" },
 ])

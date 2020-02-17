2afa2262dda77b8dab0087d396c9216395261647
Kuba Subczynski <kubasub12@gmail.com>
2020-02-17 15:08:53 -0500

    Add chart for standings over time

--- app/controllers/standings_controller.rb
+++ app/controllers/standings_controller.rb
@@ -1,5 +1,26 @@
 class StandingsController < ApplicationController
   def index
     @ranked_teams = Leaderboard.new.ranked_teams
+
+    @line_chart_data = @ranked_teams.map do |team|
+      {
+        name: team.name,
+        data: episodes.map do |episode|
+          [
+            episode.number,
+            ScoringEvent
+              .preload(:scoring_criterion)
+              .where(castaway: team.castaways, episode: episodes.where(number: 1..episode.number))
+              .sum(&:points)
+          ]
+        end.to_h
+      }
+    end
+  end
+
+  private
+
+  def episodes
+    @episodes ||= Episode.order(:number)
   end
 end
